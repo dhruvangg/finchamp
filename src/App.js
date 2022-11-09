84be3e9a52412ae7de03c29544e4ee9a088d6476
dhruvang <dhruvangg@gmail.com>
2022-11-09 12:06:50 +0530

    Added README.md

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <a>LOGO</a>
+        <nav>
+          <ul>
+            <li><a></a></li>
+            <li><a></a></li>
+            <li><a>Calculators</a></li>
+            <li><a>Contact</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>App</main>
+      <footer></footer>
     </div>
-  );
+  )
 }
-
-export default App;
