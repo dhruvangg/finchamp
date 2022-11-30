3585111558769b9388054206099417182d3c4660
dhruvang <dhruvangg@gmail.com>
2022-11-30 23:19:21 +0530

    Added Typography in tailwind

--- pages/taxation/index.jsx
+++ pages/taxation/index.jsx
@@ -1,5 +1,4 @@
-import Layout from "components/styled/Layout";
-import Main from "components/styled/Main";
+import { Container, Layout, Page } from '@/components/styled';
 import Head from 'next/head'
 import Image from "next/image";
 import Link from "next/link";
@@ -12,18 +11,20 @@ export default function Taxation() {
                 <meta name="description" content="Financial Awareness platform" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Main>
-                <h1>Taxation</h1>
-                <p>In India, There are lot of taxes deducated directly and indirectly. Among that here we will discuss about direct taxes as it impacts more on our pocket. As given in figure, There are mainly three types of taxes: </p>
-                <ul>
-                    <li><strong>Capital Gain tax</strong> Nowadays Many people invests in Mutual fund and share market, hence they might gain profit or loss. So Whatever you earn through that way is taxable and is called Capital Gain.</li>
-                    <li><strong><Link href="https://cleartax.in/s/wealth-tax">Wealth tax</Link></strong> If the total net wealth of an individual, HUF or company exceeds Rs. 30 lakhs, on the valuation date, tax @1% will be leviable on the amount in excess of Rs. 30 lakhs.  Every person whose net wealth exceeds such limit shall furnish a return of net wealth. The due date is same as that of Income tax return.</li>
-                    <li><strong><Link href="https://cleartax.in/s/income-tax">Income tax</Link></strong> is a type of tax that the central government charges on the income earned during a financial year by the individuals and businesses. </li>
-                </ul>
-                <div>
-                    <Image src="/assets/types-of-direct-tax.png" alt="types-of-direct-tax" width="1632" height="672" style={{ maxWidth: "1024px", height: "auto" }} />
-                </div>
-            </Main>
+            <Page>
+                <Container className="prose lg:prose-xl">
+                    <h1>Taxation</h1>
+                    <p>In India, There are lot of taxes deducated directly and indirectly. Among that here we will discuss about direct taxes as it impacts more on our pocket. As given in figure, There are Pagely three types of taxes: </p>
+                    <ul>
+                        <li><strong>Capital Gain tax</strong> Nowadays Many people invests in Mutual fund and share market, hence they might gain profit or loss. So Whatever you earn through that way is taxable and is called Capital Gain.</li>
+                        <li><strong><Link href="https://cleartax.in/s/wealth-tax">Wealth tax</Link></strong> If the total net wealth of an individual, HUF or company exceeds Rs. 30 lakhs, on the valuation date, tax @1% will be leviable on the amount in excess of Rs. 30 lakhs.  Every person whose net wealth exceeds such limit shall furnish a return of net wealth. The due date is same as that of Income tax return.</li>
+                        <li><strong><Link href="https://cleartax.in/s/income-tax">Income tax</Link></strong> is a type of tax that the central government charges on the income earned during a financial year by the individuals and businesses. </li>
+                    </ul>
+                    <div>
+                        <Image src="/assets/types-of-direct-tax.png" alt="types-of-direct-tax" width="1632" height="672" style={{ maxWidth: "1024px", height: "auto" }} />
+                    </div>
+                </Container>
+            </Page>
         </Layout>
     )
 }
