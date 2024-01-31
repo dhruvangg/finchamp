edc47e2c00482a1ccb797a377fee509f96c8e71d
Dhruvang Gajjar <dhruvangg@gmail.com>
2024-01-31 21:29:37 +0530

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,4 +8,4 @@ export const LANGUAGES = [{
     label: "ગુજરાતી"
 }]
 
-export const server = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://finchamp.vercel.app';
+export const server = process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'https://moneyinsightful.vercel.app';
